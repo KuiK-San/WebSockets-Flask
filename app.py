51555bb46a8024924e337ed90c95a1f246fb12a5
Guilherme Casagrande <thegui4000@gmail.com>
2023-10-09 14:35:35 -0300

    feat: fazendo requisição por post

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from flask import request
 from flask_socketio import SocketIO
 from urllib.parse import parse_qs
+from datetime import datetime
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -11,7 +12,17 @@ def log():
     data = request.data
     dt_str = data.decode('utf-8')
     data = parse_qs(dt_str)
-    print(data)
+    lat = data['lat'][0]
+    lon = data['lon'][0]
+    data_e_hora_atuais = datetime.now()
+    horario = data_e_hora_atuais.strftime("%H:%M:%S %d/%m/%Y")
+
+
+    # print(data)
+
+    socketio.emit('message', {"lat":lat, "lon": lon,"horario": horario})
+
+    # print(data)
     return jsonify({"status": "200"})
 
 @app.route('/', methods=['GET'])
