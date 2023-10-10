31540d6b3c4ddfc3d8a1666b10d20b56c8cdb5a3
Guilherme Casagrande <thegui4000@gmail.com>
2023-10-10 16:45:50 -0300

    feat: integração com o banco de dados e um doc para cada dispositivo ainda precisamos definir como será o nome dos dispositivos fazer a parte do front-end e API

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import request
 from flask_socketio import SocketIO
 from urllib.parse import parse_qs
 from datetime import datetime
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['localizador']
+collection = db['dispositivos']
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -16,6 +21,36 @@ def log():
     lon = data['lon'][0]
     data_e_hora_atuais = datetime.now()
     horario = data_e_hora_atuais.strftime("%H:%M:%S %d/%m/%Y")
+    dia = data_e_hora_atuais.strftime("%d/%m/%Y")
+
+    serial = data['ser'][0]
+    if not collection.find_one({'serial': serial}):
+        document = {
+            'serial': str(data['ser'][0]),
+            f'rota_{dia}': {
+                str(0):{
+                    'lat': float(lat),
+                    'lon': float(lon) 
+                }
+            }
+        }
+
+        collection.insert_one(document)
+
+    elif collection.find_one({'serial': serial}) and not f'rota_{dia}' in  collection.find_one({'serial': serial}):
+
+        collection.update_one({'serial': serial}, {"$set": {f"rota_{dia}.{str(0)}": {
+            'lat': float(lat),
+            'lon': float(lon)
+        }}})
+
+    else:
+        doc = collection.find_one({'serial': serial})
+        quantidade = len(doc[f'rota_{dia}'])
+        collection.update_one({'serial': serial}, {"$set": {f"rota_{dia}.{str(quantidade)}": {
+            'lat': float(lat),
+            'lon': float(lon)
+        }}})
 
     socketio.emit('message', {"lat":lat, "lon": lon,"horario": horario})
     socketio.emit('data', data)
